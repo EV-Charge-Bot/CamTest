d3a11ac1ea8b5ece44cfeb3f180204c7ba875989
KwinTran <113071385+KwinTran@users.noreply.github.com>
2023-03-24 23:59:04 -0700

    Create CamTest.py

---
+++ CamTest.py
@@ -0,0 +1,16 @@
+import cv2
+webcam=cv2.VideoCapture(0)
+
+while True:
+    ret,frame=webcam.read()
+    
+    if ret==True:
+        cv2.imshow('BGR',frame)
+
+
+        key=cv2.waitKey(1)
+        if key==ord("q"):
+            break
+        
+webcam.release()
+cv2.destroyAllWindows()
